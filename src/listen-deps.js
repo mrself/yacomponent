1ea822352728f6bb442367601f5f843a442e1463
Lightman <yaruk.my@gmail.com>
2017-08-09 15:45:49 +0300

    fix listen deps

--- src/listen-deps.js
+++ src/listen-deps.js
@@ -3,11 +3,15 @@ function ListenDeps(base, $doc, item) {
 	var deps = {};
 
 	this.init = function() {
-		item.targets.forEach(function(i) {
+		this.bindInitEvent();
+		var defs = item.targets.map(function(i) {
+			var def = $.Deferred();
 			var target = self.makeTarget(i);
 			var depName = target['class'].getName();
-			self.listenDep(depName, target);
+			self.listenDep(depName, target, def);
+			return def;
 		});
+		this.def = $.when.apply(null, defs);
 	};
 
 	this.makeTarget = function(origin) {
@@ -16,25 +20,28 @@ function ListenDeps(base, $doc, item) {
 		return target;
 	};
 
-	this.listenDep = function(depName, target) {
+	this.listenDep = function(depName, target, def) {
 		$doc.on(depName + '/init', function(e, inst) {
 			deps[depName] = inst;
-			self.check(target.name, inst.options.instance);
+			self.check(target.name, inst.options.instance, def);
 		});
 	};
 
-	this.check = function(names, instName) {
+	this.check = function(names, instName, def) {
 		if (!instName) return;
 		var index = names.indexOf(instName);
 		if (index == -1) return;
 		names.splice(index, 1);
-		if (!names.length) this.bindInitEvent();
+		if (!names.length) def.resolve();
 	};
 
 	this.bindInitEvent = function() {
 		$doc.on(base.getName() + '/init', function(e, instance) {
-			if (instance.options.instance == item.instance)
-				instance[item.method](deps);
+			if (instance.options.instance == item.instance) {
+				self.def.done(function() {
+					instance[item.method](deps);
+				});
+			}
 		});
 	};
 
